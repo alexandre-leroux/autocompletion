498c41a95fa4a9749b181acc3f11914b332553b8
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-04-15 10:38:50 +0200

    Update moteur_autocompletion.php

--- moteur/moteur_autocompletion.php
+++ moteur/moteur_autocompletion.php
@@ -3,32 +3,27 @@
 include('database.php');
 
 
-
 function moteur_de_recherche()
     {
 
+
         $bdd = connection_bdd();
+
         $motclef = $_POST['motclef'];
-        // var_dump($_POST['motclef']);
-        // echo '</br>';
+        $motclef_secure = htmlspecialchars($motclef);
 
-        $req_search = $bdd->prepare("SELECT * FROM sportifs WHERE nom_complet LIKE ? LIMIT 8 ");
-        $req_search->execute(array("%$motclef%"));
+        $req_search = $bdd->prepare("SELECT * FROM sportifs WHERE nom_complet LIKE ?  LIMIT 8 ");
+        $req_search->execute(array("%$motclef_secure%"));
         $res = $req_search->fetchAll();
+        
+        $req_search_2 = $bdd->prepare("SELECT * FROM sportifs WHERE bio LIKE ? AND nom_complet NOT LIKE ? LIMIT 8  ");
+        $req_search_2->execute(array("%$motclef_secure%","%$motclef_secure%"));
+        $res_2 = $req_search_2->fetchAll();
 
-        if($res != null)
-        {
-            echo json_encode($res);
-        }
 
-
-        else
-        {
-            $req_search = $bdd->prepare("SELECT * FROM sportifs WHERE bio LIKE ? LIMIT 8 ");
-            $req_search->execute(array("%$motclef%"));
-            $res = $req_search->fetchAll();
-            echo json_encode($res);
-        }
+        $res_total = $res + $res_2;
+        echo json_encode($res_total);
+        // return $res_total;
 
 
     }
@@ -38,13 +33,39 @@ moteur_de_recherche();
 
 
 
-    // $motclef = $_GET['key'];
-    // $motcle_secure = htmlspecialchars($motclef);
-    // $req_search = $bdd->query(" SELECT * FROM sportifs WHERE MATCH (nom,prénom,nom_complet,bio) AGAINST ('$motcle_secure' IN NATURAL LANGUAGE MODE);  ");
-    // // $req_search->execute(array("%$motclef%" ));
+// function moteur_de_recherche()
+//     {
 
-    // $res = $req_search->fetch();
-    // // var_dump($res);
-    //     // echo ($res);
-    // echo json_encode($res);
+//         $bdd = connection_bdd();
+//         $motclef = $_POST['motclef'];
+//         // var_dump($_POST['motclef']);
+//         // echo '</br>';
+
+//         $req_search = $bdd->prepare("SELECT * FROM sportifs WHERE nom_complet LIKE ? LIMIT 8 ");
+//         $req_search->execute(array("%$motclef%"));
+//         $res = $req_search->fetchAll();
+
+//         if($res != null)
+//         {
+//             echo json_encode($res);
+//         }
+
+
+//         else
+//         {
+//             $req_search = $bdd->prepare("SELECT * FROM sportifs WHERE bio LIKE ? LIMIT 8 ");
+//             $req_search->execute(array("%$motclef%"));
+//             $res = $req_search->fetchAll();
+//             echo json_encode($res);
+//         }
+
+
+//     }
+
+
+// moteur_de_recherche();
+
+
+
+
     
